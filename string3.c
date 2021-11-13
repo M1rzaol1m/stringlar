9cfa70435ecfff8aad1f92b8ce9e9ebeff0a53c2
M1rzaol1m <mirzaolimislamov98@icloud.com>
2021-11-13 20:22:59 +0500

    string 3.

---
+++ string3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("son kiriting:");
+    scanf("%d", &n);
+    char c;
+        printf ("%c ", n);
+        n++;
+        printf ("%c ", n);
+        n--;
+        printf ("%c", n);
+    return 0;
+}

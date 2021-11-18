955c2294dbc0eede0132a5bc176cd672409e799d
M1rzaol1m <mirzaolimislamov98@icloud.com>
2021-11-18 12:25:48 +0500

    string 13

---
+++ string13.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+int main()
+{
+    printf("\n");
+    char ism[100];
+
+    printf("ismingizni kiriting : ");
+    fgets(ism, 100, stdin);
+
+    int ism_olchami = strlen(ism)-1;
+    printf("ismingiz %d-ta harifdan iborat ", ism_olchami);
+    printf("\n");
+
+    return 0;
+}
